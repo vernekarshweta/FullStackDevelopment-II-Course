9ccf2f0617adc75f3a065a4ac2814c4e1e54b410
shweta <vernekarsweta@gmail.com>
2026-04-02 12:49:14 +0530

    login api

--- Exp-3-EmployeeRestAPI/server.js
+++ Exp-3-EmployeeRestAPI/server.js
@@ -6,7 +6,36 @@ app.use(express.json())
 
 const employeeRoutes = require("./routes/employeeRoutes")
 
-app.use("/employees", employeeRoutes)
+app.post("/login", (req, res) => {
+
+    const { username, password } = req.body;
+
+    // dummy check
+    if (username === "admin" && password === "123") {
+
+        const user = {
+            id: 1,
+            username: "admin"
+        };
+
+        const token = jwt.sign(user, SECRET_KEY, {
+            expiresIn: "1h"
+        });
+
+        res.json({
+            message: "Login success",
+            token: token
+        });
+
+    } else {
+        res.status(401).send("Invalid user");
+    }
+
+});
+
+
+// add routing
+app.use("/employees",verifyToken, employeeRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
